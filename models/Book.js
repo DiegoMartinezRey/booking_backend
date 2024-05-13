ad969c90ca60a0c6e0399b3cc53384df63056487
Diego Martinez <diegomrey1@gmail.com>
2024-05-13 19:48:29 +0200

    add book model

---
+++ models/Book.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema(
+  {
+    name: { type: String, require: true },
+    images: { type: Array, require: true },
+    type: { type: String, require: true },
+    role: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const bookModel = mongoose.model("book", bookSchema);
+
+module.exports = bookModel;
