01432729c3d936df688577c875e67861093facb5
Diego Martinez <diegomrey1@gmail.com>
2024-05-13 19:59:48 +0200

    add user routes

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const router = require("./routes/userRouter");
 
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/", router);
 
 app.listen(port, () => {
   console.log("Running server on Port: ", port);
