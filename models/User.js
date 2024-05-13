dad202734f62fb56cf28738c3efb64ba48a48a53
Diego Martinez <diegomrey1@gmail.com>
2024-05-13 19:44:04 +0200

    add user model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, require: true, maxlenght: 15 },
+    surname: { type: String, require: true, maxlenght: 15 },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    bookings: { type: Array },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
